03cb5a62dcb89ce4ec62ac084b40eaf6a81fa9fb
Thomas <tdupre@optae.fr>
2017-02-13 11:29:45 +0100

    -- test commit

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Plume' });
 });
 
-router.get('/apanel', function(req, res, next) {
+router.get('/apanel', function(req, res) {
   res.render('apanel', { title: 'Plume' });
+});
+
+router.post('/login', function(req, res) {
+  var username = req.param('username');
+  var password = req.password('password');
+
+  // Verify password in the DB
+
+  console.log("");
+
+  res.json({ user: 'tobi' });
 });
 
 
