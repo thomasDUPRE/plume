f95468118f9611c7ac15363405738eff53dc0398
Momodiop14 <mad@esp.sn>
2017-02-16 18:19:15 +0100

    premieres vues

--- crud/beans/Erreur.js
+++ crud/beans/Erreur.js
@@ -1,14 +1,14 @@
-/**
- * Created by Thomas on 13/02/2017.
- */
-var Enum = require('enumify').Enum;
-class TypeErreur extends Enum{}
-TypeErreur.initEnum(['ErreurConnexion']);
+// /**
+//  * Created by Thomas on 13/02/2017.
+//  */
+// var Enum = require('enumify').Enum;
+// class TypeErreur extends Enum{}
+// TypeErreur.initEnum(['ErreurConnexion']);
 
-class Erreur{
-    constructor(type, message) {
-        this.type = type;
-        this.message = message;
-    }
-}
-module.exports = Erreur;
+// class Erreur{
+//     constructor(type, message) {
+//         this.type = type;
+//         this.message = message;
+//     }
+// }
+// module.exports = Erreur;
