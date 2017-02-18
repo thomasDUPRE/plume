f3e9d22037b301a0ef5d75df30565840bcbe500c
Momodiop14 <mad@esp.sn>
2017-02-18 11:15:01 +0100

    integration vues/projet

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 
- // app.set('view engine', 'twig');
- // app.set("view options", { layout: false });
+ app.set('view engine', 'twig');
+ app.set("view options", { layout: false });
  app.set("twig options", {  strict_variables: false});
-//app.engine('html', require('ejs').renderFile);
+ //app.engine('html', require('ejs').renderFile);
 
 //Store all JS and CSS in Scripts folder.
 
@@ -29,9 +29,70 @@ app.get('/',function(req,res){
 		res.redirect('/admin');
 	}
 	else{
+		res.render('layout.twig');
+	}
+});
+
+app.get('/calendarConge',function(req,res){
+	sess=req.session;
+
+	if(sess.email)
+	{
+		res.redirect('/admin');
+	}
+	else{
+		res.render('calendar.twig');
+	}
+});
+
+app.get('/detailConge',function(req,res){
+	sess=req.session;
+
+	if(sess.email)
+	{
+		res.redirect('/admin');
+	}
+	else{
 		res.render('conges.twig');
 	}
 });
+
+app.get('/demandeConge',function(req,res){
+	sess=req.session;
+
+	if(sess.email)
+	{
+		res.redirect('/admin');
+	}
+	else{
+		res.render('demandeConge.twig');
+	}
+});
+
+app.get('/demandeNoteFrais',function(req,res){
+	sess=req.session;
+
+	if(sess.email)
+	{
+		res.redirect('/admin');
+	}
+	else{
+		res.render('demandeNFrais.twig');
+	}
+});
+
+app.get('/notesDetails',function(req,res){
+	sess=req.session;
+
+	if(sess.email)
+	{
+		res.redirect('/admin');
+	}
+	else{
+		res.render('notes_de_frais.twig',{mois:"janvier 2017"});
+	}
+});
+
 
 app.post('/login',function(req,res){
 	sess = req.session;	
