5d0609e0caad29f002f059e624ca00455cebc3a1
Thomas <thomas.dupre@u-psud.fr>
2017-02-18 16:21:44 +0100

    Front-end en place

--- routes/routes_main.js
+++ routes/routes_main.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
     console.log(__dirname);
     app.get('/', function (req, res) {
         // Homepage
-    	var path = require("path"),
-        fs = require("fs");
-        res.sendFile(path.join(__dirname, '../', 'views/login.html'));
+        var path = require("path"),
+            fs = require("fs");
+        res.sendFile(path.join(__dirname, '..', 'views/login.html'));
     });
 }
--- server.js
+++ server.js
@@ -1,140 +1,29 @@
-var express		=	require('express');
-var session		=	require('express-session');
-var bodyParser  = 	require('body-parser');
-var twig = require("twig"),
-    express = require('express'),
-    app = express();
+var express = require("express");
+var bodyParser = require('body-parser');
 
+var app     = express();
 
-app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
-app.use(bodyParser.json());      
-app.use(bodyParser.urlencoded({extended: true}));
-
-
+app.use(express.static(__dirname + '/views'));
+//Store all HTML files in view folder.
 app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
-
- app.set('view engine', 'twig');
- app.set("view options", { layout: false });
- app.set("twig options", {  strict_variables: false});
- //app.engine('html', require('ejs').renderFile);
-
 //Store all JS and CSS in Scripts folder.
 
-app.get('/',function(req,res){
-	sess=req.session;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-	if(sess.email)
-	{
-		res.redirect('/admin');
-	}
-	else{
-		res.render('layout.twig');
-	}
-});
-
-app.get('/calendarConge',function(req,res){
-	sess=req.session;
-
-	if(sess.email)
-	{
-		res.redirect('/admin');
-	}
-	else{
-		res.render('calendar.twig');
-	}
-});
-
-app.get('/detailConge',function(req,res){
-	sess=req.session;
-
-	if(sess.email)
-	{
-		res.redirect('/admin');
-	}
-	else{
-		res.render('conges.twig');
-	}
-});
-
-app.get('/demandeConge',function(req,res){
-	sess=req.session;
-
-	if(sess.email)
-	{
-		res.redirect('/admin');
-	}
-	else{
-		res.render('demandeConge.twig');
-	}
-});
-
-app.get('/demandeNoteFrais',function(req,res){
-	sess=req.session;
-
-	if(sess.email)
-	{
-		res.redirect('/admin');
-	}
-	else{
-		res.render('demandeNFrais.twig');
-	}
-});
-
-app.get('/notesDetails',function(req,res){
-	sess=req.session;
-
-	if(sess.email)
-	{
-		res.redirect('/admin');
-	}
-	else{
-		res.render('notes_de_frais.twig',{mois:"janvier 2017"});
-	}
-});
+// -- Routing
+require('./routes/routes_main')(app);
+require('./routes/routes_admin')(app);
+require('./routes/routes_conge')(app);
+require('./routes/routes_ndf')(app);
+require('./routes/routes_autres')(app);
 
 
-app.post('/login',function(req,res){
-	sess = req.session;	
-	sess.email = req.body.email;
-	console.log("email:"+req.body.email);
-	res.end('done');
-});
-
-app.get('/admin',function(req,res){
-	sess=req.session;
-	if(sess.email)	
-	{
-		res.write('<h1>Hello'+sess.email+'</h1><br>');
-		res.end('<a href='+'/logout'+'>Logout</a>');
-	}
-	else
-	{
-		res.write('<h1>Please login first.</h1>');
-		res.end('<a href='+'/'+'>Login</a>');
-	}
-
-});
-
-app.get('/logout',function(req,res){
-	
-	req.session.destroy(function(err){
-		if(err){
-			console.log(err);
-		}
-		else
-		{
-			res.redirect('/');
-		}
-	});
-
-});
 
 app.listen(3000, function (err) {
-  if (err) {
-    throw err
-  }
+
 });
 
 console.log("App Started on PORT 3000");
+
 
