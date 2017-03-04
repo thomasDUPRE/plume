d940b0af365be3551394fd0261810209064e7152
yassine_karray <yassinekarray@outlook.fr>
2017-03-04 18:27:43 +0100

    insertion suppression Conge par get toujours le soucis pour service

--- routes/routes_conge.js
+++ routes/routes_conge.js
@@ -2,6 +2,7 @@
  * Created by Thomas on 13/02/2017.
  */
 // Routes Congés
+
 module.exports = function(app) {
     var Erreur = require('../crud/beans/Erreur');
 
@@ -29,24 +30,84 @@ app.get('/selectConge', function (req, res) {
         else res.send(JSON.stringify(new Erreur("RequeteErreur", Erreur.UNDEFINED)));
 
     });
-app.get('/selectCongeService', function (req, res) {
-        // if the request exists
+
+
+    app.get('/insererConge', function (req, res) {
         if (typeof req.query !== 'undefined' && req.query) {
-            var selector = {};
-            if(doesParamExist(req.query.id)) selector.id_demandeur = parseInt(req.query.id);
-            var CongeCRUD = require('../crud/CongeCRUD');
+            // if the parameters are ok
+            if (doesParamExist(req.query.id) && doesParamExist(req.query.date_demande) &&doesParamExist(req.query.date_debut) && doesParamExist(req.query.part_matin) && doesParamExist(req.query.id_etat_conge) && doesParamExist(req.query.est_paye) && doesParamExist(req.query.revient_matin) && doesParamExist(req.query.motif) &&doesParamExist(req.query.date_fin) &&  oesParamExist(req.query.id_demandeur) ){
+                var data = {
+                    id : req.query.id,
+                    date_demande : req.query.date_demande,
+                    date_debut : req.query.date_debut,
+                    part_matin : req.query.part_matin,
+                    id_etat_conge : req.query.id_etat_conge,
+                    est_paye : req.query.est_paye,
+                    revient_matin : req.query.revient_matin,
+                    motif : req.query.motif,
+                    date_fin : req.query.date_fin,
+                    id_demandeur : req.query.id_demandeur,
+                };
+                var CongeCRUD = require('../crud/CongeCRUD');
 
-            CongeCRUD.selectCongeService(selector, function callback(result) {
-                console.log("Result :" + JSON.stringify(result));
-                // Send result to the browser
-                res.send(JSON.stringify(result));
-            })
+                CongeCRUD.insererConge(data, function callback(result){
+                    console.log("Result :" + JSON.stringify(result));
+                    // Send result to the browser
+                    res.send(JSON.stringify(result));
+                });
 
+            }
+            // Display error
+            else res.send(JSON.stringify(new Erreur("ParamErreur", Erreur.WRONG_PARAM)));
         }
         // Display Error
         else res.send(JSON.stringify(new Erreur("RequeteErreur", Erreur.UNDEFINED)));
-
     });
 
+app.get('/deleteConge', function (req, res) {
+        // if the request exists
+        if (typeof req.query !== 'undefined' && req.query) {
+            var selector = {};
+            if(doesParamExist(req.query.id)) selector.id = parseInt(req.query.id);
+            var CongeCRUD = require('../crud/CongeCRUD');
 
+            CongeCRUD.deleteConge(selector, function callback(result) {
+            // Send result to the browser
+            res.send(JSON.stringify(result));
+            })
+        }
+     
+       // Display Error
+       else res.send(JSON.stringify(new Erreur("RequeteErreur", Erreur.UNDEFINED)));
+
+    });
+app.get('/selectCongeService', function (req, res) {
+	var Conge = require('../crud/beans/Conge');
+        // if the request exists
+        if (typeof req.query !== 'undefined' && req.query) {
+            var selector = {};
+            var tmpCollaborateurs =[];
+			var tmpConges =[];
+            if(doesParamExist(req.query.id)) selector.id_demandeur = parseInt(req.query.id);
+            var CongeCRUD = require('../crud/CongeCRUD');
+            var CollaborateurCRUD = require('../crud/CollaborateurCRUD');
+
+            CongeCRUD.selectConge({}, function callback(result) {
+            	for(var i = 0, len = result.length; i < len; i++){
+                    tmpConges.push(new Conge(result[i].id, result[i].date_demande, result[i].date_debut, result[i].date_fin, result[i].est_paye, result[i].id_etat_conge, result[i].part_matin, result[i].revient_matin, result[i].motif,result[i].id_demandeur));
+                }
+
+            })
+
+                CollaborateurCRUD.selectCollaborateurs({}, function callback(result) {
+                // Send result to the browser
+              tmpCollaborateurs=result.slice(0);
+            })
+                console.log("Result :" + JSON.stringify(tmpConges));
+
+              res.send(JSON.stringify(tmpConges));
+        }
+        // Display Error
+        else res.send(JSON.stringify(new Erreur("RequeteErreur", Erreur.UNDEFINED)));
+    });
 }
